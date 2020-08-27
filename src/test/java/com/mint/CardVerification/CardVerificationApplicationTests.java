191d02ddf406f9a683d6f7cea21e547f331c403d
paschalovaga <paschalovaga@gmail.com>
2020-08-27 13:10:20 +0100

    First commit

---
+++ src/test/java/com/mint/CardVerification/CardVerificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mint.CardVerification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardVerificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
